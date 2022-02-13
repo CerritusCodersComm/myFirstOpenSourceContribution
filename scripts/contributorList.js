a25c312694d0ffe99c62f0b91e5b8ed0ed080512
Anam Ansari <anamansari062@gmail.com>
2022-02-13 18:37:42 +0530

    Added my name in json

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -1,11 +1,14 @@
-
 let contributors = [{
 
-    "github": "https://github.com/cdhiraj40",
-    "name": "DHIRAJ CHAUHAN"
-},
-{
-    "github": "https://github.com/ishir21",
-    "name": "Ishir Agrawal"
-},
+        "github": "https://github.com/cdhiraj40",
+        "name": "DHIRAJ CHAUHAN"
+    },
+    {
+        "github": "https://github.com/ishir21",
+        "name": "Ishir Agrawal"
+    },
+    {
+        "github": "https://github.com/anamansari062",
+        "name": "Anam Ansari"
+    },
 ]
