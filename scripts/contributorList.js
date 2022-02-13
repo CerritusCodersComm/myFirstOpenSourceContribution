40ce6b5aa1dd50fd40daed7a6c0134823f15632c
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-02-13 17:00:59 +0530

    added my name

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -4,10 +4,8 @@ let contributors = [{
     "github": "https://github.com/cdhiraj40",
     "name": "DHIRAJ CHAUHAN"
 },
-                    {
+{
     "github": "https://github.com/ishir21",
     "name": "Ishir Agrawal"
 },
 ]
-
-
