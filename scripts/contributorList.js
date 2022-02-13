cd0fd80d34a694fc4c6c58b3d67478eb6ca2518d
Ishir Agrawal <91480925+ishir21@users.noreply.github.com>
2022-02-13 16:55:11 +0530

    Update contributorList.js

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -3,7 +3,11 @@ let contributors = [{
 
     "github": "https://github.com/cdhiraj40",
     "name": "DHIRAJ CHAUHAN"
-}
+},
+                    {
+    "github": "https://github.com/ishir21",
+    "name": "Ishir Agrawal"
+},
 ]
 
 
