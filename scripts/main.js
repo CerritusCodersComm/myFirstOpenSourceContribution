8e227f1de808af8494e4d798c7346862e8664d54
Adnan Ahmad <viperadnan@gmail.com>
2022-02-15 23:28:37 +0530

    🚀 Using api for fetching contributors dynamically

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,28 @@
-window.onload = () => {
+let tableContent = ``;
 
-    let tableContent = ``;
-    for (let i = 0; i < contributors.length; i++) {
-        let row = `
+for (let i = 0; i < contributors.length; i++) {
+    let row = `
         <tr>
             <td>
-                <img src="./images/prLogo.svg" alt="PR logo">
+                <img src="${contributors[i].avatar_url}" alt="avatar">
             </td>
             <td>
-                <a href=${contributors[i].github} class="name" target="_blank"> ${contributors[i].name} </a>
+                <a id="user-${contributors[i].username}" href=${contributors[i].github_url} class="name" target="_blank"><b>${contributors[i].username}</b></a>
             </td>
-            <td class="rank">${i + 1}</td>
+            <td class="rank">${contributors[i].contributions}</td>
         </tr>`;
-        tableContent += row;
-    }
-    document.getElementsByTagName("table")[0].innerHTML = tableContent
+    tableContent += row;
 }
+
+document.getElementsByTagName("table")[0].innerHTML = tableContent;
+
+window.onload = function () {
+    // Updating the name of contributors
+    // Didn't this before rendering the table because it will
+    // make page loading slower af
+    for (let i = 0; i < contributors.length; i++) {
+        var data = fetchJSON(contributors[i].github_api_url);
+        console.log(data.name);
+        if (data.name) document.getElementById("user-" + contributors[i].username).innerHTML = `<b>${data.name}</b>`;
+    }
+};
