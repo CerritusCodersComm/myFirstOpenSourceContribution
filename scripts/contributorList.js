b40b14cbcca15b0ee4dbe47525b49c85838badda
Anam Ansari <anamansari062@gmail.com>
2022-03-01 12:17:43 +0530

    Added my name

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -1,27 +1,30 @@
-
 let contributors = [{
 
-    "github": "https://github.com/cdhiraj40",
-    "name": "Dhiraj Chauhan"
-},
-{
-    "github": "https://github.com/ishir21",
-    "name": "Ishir Agrawal"
-},
-{
-"github": "https://github.com/beans9926",
-    "name": "Sarthak Gulhane"
-},
-{
-    "github": "https://github.com/anamansari062",
-    "name": "Anam Ansari"
-},
-{
-    "github": "https://github.com/Aspireve",
-    "name": "Steve Fernandes"
-},
-{
-    "github": "https://github.com/Moon-Elf",
-    "name": "Umang Maurya"
-},
+        "github": "https://github.com/cdhiraj40",
+        "name": "Dhiraj Chauhan"
+    },
+    {
+        "github": "https://github.com/ishir21",
+        "name": "Ishir Agrawal"
+    },
+    {
+        "github": "https://github.com/beans9926",
+        "name": "Sarthak Gulhane"
+    },
+    {
+        "github": "https://github.com/anamansari062",
+        "name": "Anam Ansari"
+    },
+    {
+        "github": "https://github.com/Aspireve",
+        "name": "Steve Fernandes"
+    },
+    {
+        "github": "https://github.com/Moon-Elf",
+        "name": "Umang Maurya"
+    },
+    {
+        "github": "https://github.com/anamansari062",
+        "name": "Anam Ansari"
+    }
 ]
